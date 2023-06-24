eae69cf02eced5a487d516a2b59aeea9f174fe5a
ohmygook <rnrgusgh77@naver.com>
2023-06-24 20:21:32 +0900

    web board 실습

--- mini_project4/djangoProject/djangoProject/settings.py
+++ mini_project4/djangoProject/djangoProject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,8 +58,7 @@ ROOT_URLCONF = 'djangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR/'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +71,9 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 
@@ -77,8 +82,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edudb',
+        'USER': 'root',
+        'PASSWORD': 'thh13776',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
